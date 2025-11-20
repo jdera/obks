a00f3eca2e8a819c9a10611b9d957156b5b83002
obks.dev <138844491+jdera@users.noreply.github.com>
2025-11-20 09:35:53 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconRSS from "@/assets/icons/IconRSS.svg";
 import { SITE } from "@/config";
 
 interface Social {
